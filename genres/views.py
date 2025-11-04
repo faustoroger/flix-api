bd9a33eb4646210003e914235dc1b4381ecbccc2
Fausto Roger <hixian@gmail.com>
2025-11-04 00:45:17 -0300

    feat: Added POST method to genres endpoint

--- genres/views.py
+++ genres/views.py
@@ -1,16 +1,22 @@
+import json
+
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from genres.models import Genre
 
 
+@csrf_exempt
 def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{"id": genre.id, "name": genre.name} for genre in genres]
-    print(data)
-    return JsonResponse(data, safe=False)
-
-    # resp = JsonResponse(list(genres.values()), safe=False)
-    # print(resp.content)
-    # return resp
-
-    # return JsonResponse({"genres": list(genres.values())})
+    if request.method == "GET":
+        genres = Genre.objects.all()
+        data = [{"id": genre.id, "name": genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    elif request.method == "POST":
+        data = json.loads(request.body.decode("utf-8"))
+        new_genre = Genre(name=data["name"])
+        new_genre.save()
+        return JsonResponse(
+            {"id": new_genre.id, "name": new_genre.name},
+            status=201,
+        )
