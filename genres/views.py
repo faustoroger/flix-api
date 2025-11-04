8bbb734b6e1a9d5a555bc482b9a4f0d51e6a023d
Fausto Roger <hixian@gmail.com>
2025-11-04 17:33:44 -0300

    feat: Added PUT method to genres endpoint

--- genres/views.py
+++ genres/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
 from genres.models import Genre
@@ -19,4 +20,20 @@ def genre_create_list_view(request):
         return JsonResponse(
             {"id": new_genre.id, "name": new_genre.name},
             status=201,
+        )
+
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+
+    if request.method == "GET":
+        data = {"id": genre.id, "name": genre.name}
+        return JsonResponse(data)
+    elif request.method == "PUT":
+        data = json.loads(request.body.decode("utf-8"))
+        genre.name = data["name"]
+        genre.save()
+        return JsonResponse(
+            {"id": genre.id, "name": genre.name},
         )
