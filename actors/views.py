4c6bb6b66a7414ca4f7f02d16f4be05986446f11
Fausto Roger <hixian@gmail.com>
2025-11-08 21:38:16 -0300

    feat: Added create endpoint to actors app

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from actors.models import Actor
+from actors.serializers import ActorSerialize
+
+
+class ActorCreateListView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerialize
+
+
+class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerialize
