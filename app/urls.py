aeebedb976114485e3b41c137da1b76abddc40ce
Fausto Roger <hixian@gmail.com>
2025-11-11 18:06:41 -0300

    feat: Added CRUD to reviews app

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,5 +25,11 @@ urlpatterns = [
         "movies/<int:pk>",
         MovieRetrieveUpdateDestroyView.as_view(),
         name="movie-detail-view",
+    ),
+    path("reviews/", ReviewCreateListView.as_view(), name="review-create-list"),
+    path(
+        "reviews/<int:pk>",
+        ReviewRetrieveUpdateDestroyView.as_view(),
+        name="review-detail-view",
     ),
 ]
