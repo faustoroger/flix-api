7ea61860a4e03a782b024648f3d5aa8d7a7d5e63
Fausto Roger <hixian@gmail.com>
2025-11-19 09:17:12 -0300

    feat: Added global permissions

--- actors/views.py
+++ actors/views.py
@@ -3,15 +3,16 @@ from rest_framework.permissions import IsAuthenticated
 
 from actors.models import Actor
 from actors.serializers import ActorSerialize
+from app.permissions import GlobalDefaultPermission
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Actor.objects.all()
     serializer_class = ActorSerialize
 
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Actor.objects.all()
     serializer_class = ActorSerialize
