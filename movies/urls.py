f13f6590106aeb9ff2c35c05306d7d76eca31a9f
Fausto Roger <hixian@gmail.com>
2025-11-19 09:14:04 -0300

    fix: URL path fixed

--- movies/urls.py
+++ movies/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("movies/", views.MovieCreateListView.as_view(), name="movie-create-list"),
     path(
-        "movies/<int:pk>",
+        "movies/<int:pk>/",
         views.MovieRetrieveUpdateDestroyView.as_view(),
         name="movie-detail-view",
     ),
--- reviews/urls.py
+++ reviews/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("reviews/", views.ReviewCreateListView.as_view(), name="review-create-list"),
     path(
-        "reviews/<int:pk>",
+        "reviews/<int:pk>/",
         views.ReviewRetrieveUpdateDestroyView.as_view(),
         name="review-detail-view",
     ),
